7bd959450a143ef6cef6178ad36a71165a60f81e
Skywings1605 <skywings.mediacom@gmail.com>
2023-03-01 14:10:28 +0530

    feat: pdfjs installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+// import "./styles.css";
+import Viewer from "./components/Viewer";
 
-function App() {
+export default function App() {
+  const [page, setPage] = useState(1);
+  const windowRef = useRef();
+  const url = "https://api.printnode.com/static/test/pdf/multipage.pdf";
+
+  const scrollToItem = () => {
+    windowRef.current && windowRef.current.scrollToItem(page - 1, "start");
+  };
+
   return (
     <div className="App">
-      <h1>WELCOME</h1>
+      <div>
+        <button>Load PDF</button>
+      </div>
+      <h1>Pdf Viewer</h1>
+      <div>
+        <input value={page} onChange={(e) => setPage(e.target.value)} />
+        <button type="button" onClick={scrollToItem}>
+          goto
+        </button>
+      </div>
+      <br />
+      <Viewer url={url} windowRef={windowRef} />
     </div>
   );
 }
-
-export default App;
