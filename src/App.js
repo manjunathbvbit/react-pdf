4eb5099c1aeb3fb7a33a5aa1f2feb59a6c7b027e
Skywings1605 <skywings.mediacom@gmail.com>
2023-03-03 14:53:28 +0530

    fix: fixed the errors of pdflib path

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React, { useState, useRef } from "react";
-// import "./styles.css";
-import Viewer from "./components/Viewer";
+import { Viewer } from "./components/Viewer";
 
 export default function App() {
-  const [page, setPage] = useState(1);
-  const windowRef = useRef();
-  const url = "https://api.printnode.com/static/test/pdf/multipage.pdf";
 
-  const scrollToItem = () => {
-    windowRef.current && windowRef.current.scrollToItem(page - 1, "start");
-  };
 
   return (
     <div className="App">
-      <div>
-        <button>Load PDF</button>
-      </div>
-      <h1>Pdf Viewer</h1>
-      <div>
-        <input value={page} onChange={(e) => setPage(e.target.value)} />
-        <button type="button" onClick={scrollToItem}>
-          goto
-        </button>
-      </div>
-      <br />
-      <Viewer url={url} windowRef={windowRef} />
+      <Viewer></Viewer>
     </div>
   );
 }
--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,51 +1,45 @@
-import React, { useEffect, useState, useCallback, useRef } from "react";
-import PropTypes from "prop-types";
-import pdfjs from "pdfjs-dist";
-import PdfViewer from "./PdfViewer";
+import { useEffect, useRef } from 'react';
 
-const Viewer = props => {
-  const { url, ...others } = props;
+import * as pdfjsLib from 'pdfjs-dist';
 
-  const pdfRef = useRef();
+export const Viewer = () => {
+    const canvasRef = useRef(null);
+    console.log("Pdf: ", pdfjsLib)
+    
+    pdfjsLib.GlobalWorkerOptions.workerSrc ='./pdf.worker.js';
+    useEffect(() => {
+        const url = '/sample.pdf';
+        const loadingTask = pdfjsLib.getDocument(url);
+        // const pdf = await pdfjsLib.getDocument('./sample.pdf').promise;
+        (async () => {
+            
+            const pdf = await loadingTask.promise;
+          const page = await pdf.getPage(1);
+          const scale = 1.5;
+          const viewport = page.getViewport({ scale });
+        
+          const outputScale = window.devicePixelRatio || 1;
+      
+        // const canvas = canvasRef.current;
+          const canvas = document.getElementById("the-canvas");
+          const context = canvas.getContext("2d");
+      
+          canvas.width = Math.floor(viewport.width * outputScale);
+          canvas.height = Math.floor(viewport.height * outputScale);
+        //   canvas.style.width = Math.floor(viewport.width) + "px";
+        //   canvas.style.height = Math.floor(viewport.height) + "px";
+      
+          const transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
+    
+          const renderContext = {canvasContext: context, transform, viewport};
+          page.render(renderContext);
+        })();
+    }, [])
 
-  const [itemCount, setItemCount] = useState(0);
-
-  useEffect(() => {
-    var loadingTask = pdfjs.getDocument(url);
-    loadingTask.promise.then(
-      pdf => {
-        pdfRef.current = pdf;
-
-        setItemCount(pdf._pdfInfo.numPages);
-
-        // Fetch the first page
-        var pageNumber = 1;
-        pdf.getPage(pageNumber).then(function(page) {
-          console.log("Page loaded");
-        });
-      },
-      reason => {
-        // PDF loading error
-        console.error(reason);
-      }
-    );
-  }, [url]);
-
-  const handleGetPdfPage = useCallback(index => {
-    return pdfRef.current.getPage(index + 1);
-  }, []);
-
-  return (
-    <PdfViewer
-      {...others}
-      itemCount={itemCount}
-      getPdfPage={handleGetPdfPage}
-    />
-  );
-};
-
-Viewer.propTypes = {
-  url: PropTypes.string.isRequired
-};
-
-export default Viewer;
+    return (
+        <div>
+            <h1>Wel come to Viewer page</h1>
+            <canvas id="the-canvas" style={{border: "1px solid black", direction: "ltr",}}></canvas>
+        </div>
+    )
+}
